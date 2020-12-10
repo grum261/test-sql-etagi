91f9f543afa54e09ee407672b242b1a821872379
grum261 <38054569+grum261@users.noreply.github.com>
2020-12-10 14:27:51 +0500

    Update less_than_avg.sql

--- 2/less_than_avg.sql
+++ 2/less_than_avg.sql
@@ -1,4 +1,4 @@
-select object_id, round(price/area_total, 2) as avg from objects
-where status = 'active'
-and class = 'Вторичная'
-and round(price/area_total, 2) >= (select avg_price from avg_square);
+select o.object_id, round(price/area_total, 2) as avg from objects o
+inner join avg_price_square_city a on o.city_id = a.city_id
+where o.action = 'Продажа' and o.status = 'active' and o.class = 'Вторичная'
+and round(price/area_total, 2) <= a.avg_price;
